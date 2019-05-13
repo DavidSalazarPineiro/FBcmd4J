37ca034725d29df4edb6d75d23a965d481fb6268
David Salazar <davidalexandro96@gmail.com>
2019-05-12 20:24:35 -0500

    modificaciones a main

--- fbcdm4j/src/fbcdm4j/Main.java
+++ fbcdm4j/src/fbcdm4j/Main.java
@@ -6,148 +6,127 @@ import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Properties;
 import java.util.Scanner;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import facebook4j.Facebook;
 import facebook4j.FacebookException;
 import facebook4j.Post;
 import facebook4j.ResponseList;
 import utils.Utils;
 
-public class Main {
-	static final Logger logger = LogManager.getLogger(Main.class);
+	public class Main {
+		static final Logger logger = LogManager.getLogger(Main.class);
+		private static final String CONFIG_DIR = "config";
+		private static final String CONFIG_FILE = "fbcmd4j.properties";
 
-	private static final String CONFIG_DIR = "config";
-	private static final String CONFIG_FILE = "fbcmd4j.properties";
+		public static void main(String[] args) {
+			logger.info("Iniciando app");
+			Facebook fb =  null;
+			Properties props = null;
 
-	public static void main(String[] args) 
-	{
-		logger.info("Iniciando app");
-		Facebook fb =  null;
-		Properties props = null;
-		int opcion = 0;
-
-		try 
-		{
-			props = Utils.loadConfigFile(CONFIG_DIR, CONFIG_FILE);
-		} catch (IOException ex) 
-		{
-			logger.error(ex);
-		}
-	
-		try 
-		{
-			Scanner scanner = new Scanner(System.in);
-			while(true) 
-			{
-				fb = Utils.configFacebook(props);
-				System.out.println("Facebook Console Client \n");
-				System.out.println("(1) Obtener NewsFeed");
-				System.out.println("(2) Obtener Wall");
-				System.out.println("(3) Publicar nuevo estado");
-				System.out.println("(4) Publicar link");
-				System.out.println("(5) Log out");
-				System.out.println("Select and option:");	
-				
-				try {
-					opcion= scanner.nextInt();
-					scanner.nextLine();
-					switch (opcion) 
-					{
-					case 1:
-						System.out.println(" |- NewsFeed  -|");
-						ResponseList<Post> newsFeed = fb.getFeed();
-						for (Post p : newsFeed) 
-						{
-							Utils.printPost(p);
-						}
-						SaveTxt("NewsFeed", newsFeed,scanner);
-						break;
-					case 2:
-						System.out.println(" |- Show Wall -|");
-						ResponseList<Post> wall = fb.getPosts();
-						for (Post p : wall) 
-						{
-							Utils.printPost(p);
-						}		
-						SaveTxt("Wall", wall, scanner);
-						break;
-					case 3:
-						System.out.println("-- Create Post --: ");
-						String estado = scanner.nextLine();
-						Utils.postStatus(estado, fb);
-						break;
-					case 4:
-						System.out.println("-- Post Url --: ");
-						String link = scanner.nextLine();
-						Utils.postLink(link, fb);
-						break;
-					case 5:
-						System.out.println("Salir");
-						System.exit(0);
-						break;
-					default:
-						logger.error("Opción inválida");
-						break;
-					}
-				} 
-				catch (InputMismatchException e) 
-				{
-					System.out.println("Ocurrió un error, revisar log.");
-					logger.error("Opción inválida. %s. \n", e.getClass());
-				} 
-				catch (FacebookException e) 
-				{
-					System.out.println("Ocurrió un error, revisar log.");
-					logger.error(e.getErrorMessage());
-				} 
-				catch (Exception e) 
-				{
-					System.out.println("Ocurrió un error, revisar log.");
-					logger.error(e);
-				}
-				System.out.println();
+			try {
+				props = Utils.loadConfigFile(CONFIG_DIR, CONFIG_FILE);
+			} catch (IOException ex) {
+				logger.error(ex);
 			}
-		} 
-		catch (Exception ex) {
-			logger.error(ex);
-		}
-	}
-	
-	
-	public static void SaveTxt(String fileName, ResponseList<Post> posts, Scanner scanner) 
-	{
-		System.out.println("¿Quieres guardar lo mostrado en un archivo txt?");
-		String opcion= scanner.nextLine();
-		if (opcion.contains("Si") || opcion.contains("si"))
-		{
-		List<Post> post = new ArrayList<>();
-		int num = 0;
-		while(num <= 0) 
-		{
-			try 
-			{
-			System.out.println("¿Cuantas lineas quieres guaradar?");
-			num = Integer.parseInt(scanner.nextLine());					
-			if(num <= 0) 
-			{
-				System.out.println("Ingrese un numero valido!!!!!");
-			} 
-			else 
-			{
-				for(int i = 0; i<num; i++)
-				{
-					if(i>posts.size()-1) break;
-					post.add(posts.get(i));
+			
+			int option = 1;
+			try {
+				Scanner scan = new Scanner(System.in);
+				while(true) {
+					fb = Utils.configFacebook(props);
+					System.out.println("Cliente de Facebook \n\n"
+									+  "Opciones: \n"
+									+  "(1) NewsFeed \n"
+									+  "(2) Wall \n"
+									+  "(3) Publicar Estado \n"
+									+  "(4) Publicar Link \n"
+									+  "(5) Salir \n"
+									+  "\nPor favor ingrese una opción:");
+					try {
+						option = scan.nextInt();
+						scan.nextLine();
+						switch (option) {
+						case 1:
+							System.out.println("Mostrando NewsFeed...");
+							ResponseList<Post> newsFeed = fb.getFeed();
+							for (Post p : newsFeed) {
+								Utils.printPost(p);
+							}
+							askToSaveFile("NewsFeed", newsFeed, scan);
+							break;
+						case 2:
+							System.out.println("Mostrando Wall...");
+							ResponseList<Post> wall = fb.getPosts();
+							for (Post p : wall) {
+								Utils.printPost(p);
+							}		
+							askToSaveFile("Wall", wall, scan);
+							break;
+						case 3:
+							System.out.println("Escribe tu estado: ");
+							String estado = scan.nextLine();
+							Utils.postStatus(estado, fb);
+							break;
+						case 4:
+							System.out.println("Ingresa el link: ");
+							String link = scan.nextLine();
+							Utils.postLink(link, fb);
+							break;
+						case 5:
+							System.out.println("Gracias!");
+							System.exit(0);
+							break;
+						default:
+							break;
+						}
+					} catch (InputMismatchException ex) {
+						System.out.println("Ocurrió un error, revisar log.");
+						logger.error("Opción inválida. %s. \n", ex.getClass());
+					} catch (FacebookException ex) {
+						System.out.println("Ocurrió un error, revisar log.");
+						logger.error(ex.getErrorMessage());
+					} catch (Exception ex) {
+						System.out.println("Ocurrió un error, revisar log.");
+						logger.error(ex);
 					}
+					System.out.println();
 				}
-			} 
-			catch(NumberFormatException e) 
-			{
+			} catch (Exception e) {
 				logger.error(e);
 			}
 		}
-	Utils.Save_Post(fileName, post);
-	}
-  }
+		
+		public static void askToSaveFile(String fileName, ResponseList<Post> posts, Scanner scan) {
+			System.out.println("Guardar resultados en un txt? Si/No");
+			String option = scan.nextLine();
+			
+			if (option.contains("Si") || option.contains("si")) {
+				List<Post> ps = new ArrayList<>();
+				int n = 0;
+
+				while(n <= 0) {
+					try {
+						System.out.println("Cuántos posts quieres guardar?");
+						n = Integer.parseInt(scan.nextLine());					
+				
+						if(n <= 0) {
+							System.out.println("Ingresa un número válido");
+						} else {
+							for(int i = 0; i<n; i++) {
+								if(i>posts.size()-1) break;
+								ps.add(posts.get(i));
+							}
+						}
+					} catch(NumberFormatException e) {
+						logger.error(e);
+					}
+				}
+
+				Utils.savePostsToFile(fileName, ps);
+			}
+		}
 }
+	
--- fbcdm4j/src/utils/Utils.java
+++ fbcdm4j/src/utils/Utils.java
@@ -1,120 +1,223 @@
 package utils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
 import facebook4j.Facebook;
 import facebook4j.FacebookException;
 import facebook4j.FacebookFactory;
 import facebook4j.auth.AccessToken;
 import facebook4j.Post;
+import facebook4j.internal.org.json.JSONObject;
 
-public class Utils 
-{
+public class Utils {
 	private static final Logger logger = LogManager.getLogger(Utils.class);
-	public static Properties loadConfigFile(String folder_name, String file_name) 
-			throws IOException 
-	{
-		Properties properties = new Properties();
-		Path configFile = Paths.get(folder_name, file_name);
-		properties.load(Files.newInputStream(configFile));
-		BiConsumer<Object, Object> emptyProperty = (k, v) -> 
-		{
+	
+	public static Properties loadConfigFile(String folderName, String fileName) throws IOException {
+		Properties props = new Properties();
+		Path configFolder = Paths.get(folderName);
+		Path configFile = Paths.get(folderName, fileName);
+		if (!Files.exists(configFile)) {
+			logger.info("Creando nuevo archivo de configuración.");
+			
+			if (!Files.exists(configFolder))
+				Files.createDirectory(configFolder);
+			
+			Files.copy(Utils.class.getResourceAsStream("fbcmd4j.properties"), configFile);
+		}
+
+		props.load(Files.newInputStream(configFile));
+		BiConsumer<Object, Object> emptyProperty = (k, v) -> {
 			if(((String)v).isEmpty())
-				logger.info("Empty propertie '" + k + "' ");
+				logger.info("La propiedad '" + k + "' está vacía");
 		};
-		properties.forEach(emptyProperty);
-		return properties;
-	}
+		props.forEach(emptyProperty);
 
+		return props;
+	}
+	
+	public static void configTokens(String folderName, String fileName, Properties props, Scanner scanner) {
+		if (props.getProperty("oauth.appId").isEmpty() || props.getProperty("oauth.appSecret").isEmpty()) {
+			System.out.println("Por favor ingrese appId:");
+			props.setProperty("oauth.appId", scanner.nextLine());
+			System.out.println("Por favor ingrese appSecret:");
+			props.setProperty("oauth.appSecret", scanner.nextLine());
+		}
 
-	public static Facebook configFacebook(Properties properties) 
-	{
-		Facebook facebook_instance = new FacebookFactory().getInstance();
-		facebook_instance.setOAuthAppId(properties.getProperty("oauth.appId"), properties.getProperty("oauth.appSecret"));
-		facebook_instance.setOAuthPermissions(properties.getProperty("oauth.permissions"));
-		facebook_instance.setOAuthAccessToken(new AccessToken(properties.getProperty("oauth.accessToken"), null));
-		return facebook_instance;
-	}
+		try {
+			URL url = new URL("https://graph.facebook.com/v2.6/device/login");
+	        Map<String,Object> params = new LinkedHashMap<>();
+	        params.put("access_token", "685875968536112|dWo3g-px6alDz6HrxzcR3meJzrM");
+	        params.put("scope", props.getProperty("oauth.permissions"));
+
+	        StringBuilder postData = new StringBuilder();
+	        for (Map.Entry<String,Object> param : params.entrySet()) {
+	            if (postData.length() != 0) postData.append('&');
+	            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+	            postData.append('=');
+	            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+	        }
+	        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+	        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+	        conn.setRequestMethod("POST");
+	        conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+	        conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+	        conn.setDoOutput(true);
+	        conn.getOutputStream().write(postDataBytes);
+
+	        Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+	        StringBuilder sb = new StringBuilder();
+	        for (int c; (c = in.read()) >= 0;)
+	            sb.append((char)c);
+	        String response = sb.toString();
+	        
+	        JSONObject obj = new JSONObject(response);
+	        String code = obj.getString("code");
+	        String userCode = obj.getString("user_code");
+	        
+			System.out.println("Ingresa a la página https://www.facebook.com/device con el código: " + userCode);
+
+			String accessToken = "";
+			while(accessToken.isEmpty()) {
+		        try {
+		            TimeUnit.SECONDS.sleep(5);
+		        } catch (InterruptedException e) {
+					logger.error(e);
+		        }
+
+		        URL url1 = new URL("https://graph.facebook.com/v2.6/device/login_status");
+		        params = new LinkedHashMap<>();
+		        params.put("access_token", "685875968536112|dWo3g-px6alDz6HrxzcR3meJzrM");
+		        params.put("code", code);
 	
-	public static void printPost(Post posts_) 
-	{
-		if(posts_.getStory() != null)
-		{
-			System.out.println("Story: " + posts_.getStory());
-		}
-		if(posts_.getMessage() != null)
-		{
-			System.out.println("Mensaje: " + posts_.getMessage());
-		}
-		System.out.println(". . . . . . . . . . . . . . . . . .");
-	}
+		        postData = new StringBuilder();
+		        for (Map.Entry<String,Object> param : params.entrySet()) {
+		            if (postData.length() != 0) postData.append('&');
+		            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+		            postData.append('=');
+		            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+		        }
+		        postDataBytes = postData.toString().getBytes("UTF-8");
 	
-	
-	public static void postLink(String link, Facebook fb) {
-		try 
-		{
-			fb.postLink(new URL(link));
-		} 
-		catch (MalformedURLException e) 
-		{
+		        HttpURLConnection conn1 = (HttpURLConnection)url1.openConnection();
+		        conn1.setRequestMethod("POST");
+		        conn1.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+		        conn1.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+		        conn1.setDoOutput(true);
+		        conn1.getOutputStream().write(postDataBytes);
+
+		        try {
+		        	in = new BufferedReader(new InputStreamReader(conn1.getInputStream(), "UTF-8"));
+			        sb = new StringBuilder();
+			        for (int c; (c = in.read()) >= 0;)
+			            sb.append((char)c);		        
+			        response = sb.toString();
+			        
+			        obj = new JSONObject(response);
+			        accessToken = obj.getString("access_token");
+		        } catch(IOException ignore) {
+		        }
+		    }
+			
+	        props.setProperty("oauth.accessToken", accessToken);
+	        
+			saveProperties(folderName, fileName, props);
+			System.out.println("Configuración guardada exitosamente.");
+			logger.info("Configuración guardada exitosamente.");
+		} catch(Exception e) {
 			logger.error(e);
-		} 
-		catch (FacebookException e) 
-		{
-		logger.error(e);
 		}
+	}
+
+	public static void saveProperties(String folderName, String fileName, Properties props) throws IOException {
+		Path configFile = Paths.get(folderName, fileName);
+		props.store(Files.newOutputStream(configFile), "Generado por org.fbcmd4j.configTokens");
+	}
+
+	public static Facebook configFacebook(Properties props) {
+		Facebook fb = new FacebookFactory().getInstance();
+		fb.setOAuthAppId(props.getProperty("oauth.appId"), props.getProperty("oauth.appSecret"));
+		fb.setOAuthPermissions(props.getProperty("oauth.permissions"));
+		if(props.getProperty("oauth.accessToken") != null)
+			fb.setOAuthAccessToken(new AccessToken(props.getProperty("oauth.accessToken"), null));
+		
+		return fb;
+	}
+	
+	public static void printPost(Post p) {
+		if(p.getStory() != null)
+			System.out.println("Story: " + p.getStory());
+		if(p.getMessage() != null)
+			System.out.println("Mensaje: " + p.getMessage());
+		System.out.println("--------------------------------");
 	}
 	
 	public static void postStatus(String msg, Facebook fb) {
-		try 
-		{
+		try {
 			fb.postStatusMessage(msg);
-		} 
-		catch (FacebookException e) 
-		{
+		} catch (FacebookException e) {
 			logger.error(e);
 		}		
 	}
 	
-	public static String Save_Post(String fileName, List<Post> posts) 
-	{
-		File file = new File(fileName + ".txt");
-		try 
-		{
-			if(!file.exists()) 
-			{ 
-				file.createNewFile(); 
-			}
-		
-		FileOutputStream f = new FileOutputStream(file);
-  		
-		for (Post p : posts) 
-		{
-			String m = "";
-			if(p.getStory() != null)
-			m += "Story: " + p.getStory() + "\n";
-			if(p.getMessage() != null)
-			m += "Mensaje: " + p.getMessage() + "\n" + ". . . . . . . . . . . . . . . . . . . .\n";
-			f.write(m.getBytes());	
+	public static void postLink(String link, Facebook fb) {
+		try {
+			fb.postLink(new URL(link));
+		} catch (MalformedURLException e) {
+			logger.error(e);
+		} catch (FacebookException e) {
+			logger.error(e);
 		}
-			f.close();
-			logger.info("Filepath name: '" + file.getName() + "'.");
-			System.out.println("Filepath name '" + file.getName() + "'.");
-		} 
-		
-		catch (IOException ex) {logger.error(ex);
-	}	
+	}
+	
+	public static String savePostsToFile(String fileName, List<Post> posts) {
+		File file = new File(fileName + ".txt");
+
+		try {
+    		if(!file.exists()) {
+    			file.createNewFile();
+            }
+
+    		FileOutputStream fos = new FileOutputStream(file);
+			for (Post p : posts) {
+				String msg = "";
+				if(p.getStory() != null)
+					msg += "Story: " + p.getStory() + "\n";
+				if(p.getMessage() != null)
+					msg += "Mensaje: " + p.getMessage() + "\n";
+				msg += "--------------------------------\n";
+				fos.write(msg.getBytes());
+			}
+			fos.close();
+
+			logger.info("Posts guardados en el archivo '" + file.getName() + "'.");
+			System.out.println("Posts guardados exitosamente en '" + file.getName() + "'.");
+		} catch (IOException e) {
+			logger.error(e);
+		}
+        
         return file.getName();
-  }	
+	}	
 }
